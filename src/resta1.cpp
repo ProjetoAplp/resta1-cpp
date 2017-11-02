0ca971164c661a1d771ab98968d9bf59e3aec0f0
alessandroliafook <alessandro.liafook@gmail.com>
2017-11-02 19:17:16 -0300

    Corrigindo erro de ;

--- src/resta1.cpp
+++ src/resta1.cpp
@@ -76,7 +76,7 @@ int main(){
     cout << "Resta 1" << EOL;
     cout << "REGRAS:" << EOL; 
     cout << EOL << "O objetivo é deixar apenas uma peça no tabuleiro após uma sequência de movimentos válidos." << EOL;    
-    cout << EOL << "O tabuleiro possui um espaço vazio no centro, com um número de peças que designam uma estrutura pré-definida."
+    cout << EOL << "O tabuleiro possui um espaço vazio no centro, com um número de peças que designam uma estrutura pré-definida.";
     cout << "O jogo apresenta duas formas de tabuleiro, a primeira com padrão inglês com 32 peças e a outra o padrão europeu com 36 peças." << EOL;
     cout << EOL << "Um movimento consiste em pegar uma peça e fazê-la 'saltar' sobre outra peça, sempre na horizontal ou na vertical, terminando em um espaço vazio adjacente a peça 'saltada'. A peça que foi 'saltada' é retirada do tabuleiro." << EOL;
     cout << EOL << "\nSelecione o tipo de tabuleiro digitando o numero correspondente: 0:INGLES ou 1:EUROPEU." << EOL;
